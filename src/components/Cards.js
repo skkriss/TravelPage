872fd161a32edc944fe75e0aa2000dfa590442db
skkriss <krystianwos67@gmail.com>
2021-02-11 22:43:14 +0100

    Navbar Links added

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -10,17 +10,17 @@ function Cards() {
                 <div className='cards__wrapper'>
                     <ul className='cards__items'>
                         <CardItem src='images/img-9.jpg' text='Zwiedź ukryty wodospad w głębi Amazońskiej Dzungli!' 
-                        label='Przygoda' parh='/services'/>
+                        label='Przygoda' path='/services'/>
                         <CardItem src='images/img-2.jpg' text='Zwiedź wyspy Bali!' 
-                        label='Luksus' parh='/services'/>
+                        label='Luksus' path='/services'/>
                     </ul>
                     <ul className='cards__items'>
                         <CardItem src='images/img-3.jpg' text='Wypłyń daleko w ocean!' 
-                        label='Zagadka' parh='/services'/>
+                        label='Zagadka' path='/products'/>
                         <CardItem src='images/img-4.jpg' text='Coś dla kochających piłkę nożną!' 
-                        label='Przygoda' parh='/services'/>
+                        label='Przygoda' path='/products'/>
                         <CardItem src='images/img-8.jpg' text='Nie lubisz zimna?!' 
-                        label='Adrenalina' parh='/services'/>
+                        label='Adrenalina' path='/products'/>
                     </ul>
                 </div>
             </div>
